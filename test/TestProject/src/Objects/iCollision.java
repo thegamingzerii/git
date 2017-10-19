d19481f86d61a450d6066ef335a08b464d902086
MoeJoe111 <32882329+MoeJoe111@users.noreply.github.com>
2017-10-19 16:47:59 +0200

    1

---
+++ test/TestProject/src/Objects/iCollision.java
@@ -0,0 +1,20 @@
+package Objects;
+
+import java.util.Vector;
+
+public interface iCollision {
+	
+	
+	void checkcollision(iCollision C);
+	
+	void getX();
+	
+	void getY();
+	
+	void setX(int x);
+	
+	void setY(int y);
+	
+	void getSize(int[] Koordinaten);
+
+}
