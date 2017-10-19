cfab43f4a02aac0e9e902e9e046808aeaddfdc38
MoeJoe111 <32882329+MoeJoe111@users.noreply.github.com>
2017-10-19 16:52:21 +0200

    1

---
+++ test/TestProject/src/Objects/ICollision.java
@@ -0,0 +1,20 @@
+package Objects;
+
+public interface ICollision {
+	
+	
+	boolean checkcollision(ICollision C);
+	
+	double getX();
+	
+	double getY();
+	
+	void setX(int x);
+	
+	void setY(int y);
+	
+	int[] getSize();
+	
+	
+
+}
