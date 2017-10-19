eac864597781df0150c340e45286c380a1b16242
thegamingzerii <31939060+thegamingzerii@users.noreply.github.com>
2017-10-19 15:35:49 +0200

    test

--- test/TestProject/src/de/thegamingzerii/maingame/Game.java
+++ test/TestProject/src/de/thegamingzerii/maingame/Game.java
@@ -86,6 +86,7 @@ public class Game extends JPanel{
 	public Game run() {
 		System.out.println("App version " + Constantes.version);
 		System.out.println("Hallo");
+		System.out.println("Moin");
 		init();
 
 		return this;
